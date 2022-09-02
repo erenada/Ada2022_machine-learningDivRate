ff3358e2e2c6a26a461a4cc26e75fc7f0dd33914
erenada <erenada@gmail.com>
2022-09-02 14:34:43 -0400

    tm_run

---
+++ Rfolder/TM_tess.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name="TM_test"
+#SBATCH --time=200:00:00  # walltime limit (HH:MM:SS)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --ntasks-per-node=20   # CHANGE THIS to processor core(s) per node
+#SBATCH --mail-user="erenada@uri.edu" #CHANGE THIS to your user email address
+#SBATCH --mail-type=ALL
+#SBATCH -o %x_%A.out
+#SBATCH -e %x_%A.err
+#SBATCH --exclusive
+
+module purge
+
+module load R/4.2.0-foss-2021b
+
+Rscript TM_tess_loop_chr1
